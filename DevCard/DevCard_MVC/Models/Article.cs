4445d2348b82be74b79e594bfa36553308cd15d2
mostafa salehi <mostafa.salehei.1362@gmail.com>
2022-07-06 12:16:32 +0430

    Projects Section From Index Page Moved To Partial View Project

---
+++ DevCard/DevCard_MVC/Models/Article.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DevCard_MVC.Models
+{
+
+    public class Article
+    {
+        public long Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+
+
+    }
+
+}
---
+++ DevCard/DevCard_MVC/Models/IndexModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DevCard_MVC.Models
+{
+    public class IndexModel
+    {
+        public List<Project> Projects { get; set; }
+        public List<Article> Articles { get; set; }
+    }
+}
